0f2b9d9ff698e68d6cce176bb8d1969fa7cc6f84
Sumit Solanki <sumit.solanki@mindbodyonline.com>
2022-01-16 12:55:10 +0530

    summary initial layout

---
+++ src/Home/PredictorWidget.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const PredictorWidget = () => {
+
+    return(
+        <div className="widget-wrapper">
+            
+            <div className="widget-head">
+            Smart predictor
+            </div>
+            <div className="widget-content">
+            content
+            </div>
+        </div>
+    )
+
+};
+export default PredictorWidget
--- src/Home/Summary.js
+++ src/Home/Summary.js
@@ -1,16 +1,100 @@
-import React from 'react';
+import React from "react";
+import Paper from "@mui/material/Paper";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+import CardImg from "./CardSvg.svg";
+import PredictorWidget from './PredictorWidget';
 
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  padding: "20px",
+  textAlign: "left",
+  // height: 60,
+  // lineHeight: '60px',
+}));
+const Summary = () => {
+  const renderDataCard = () => {
+    return (
+      <div className="single-card">
+        <div className="card-left">
+          <div className="data-title">20%</div>
+          <div className="data-detail">Lead conversion score</div>
+        </div>
+        <div className="card-right">
+          <img src={CardImg} />
+        </div>
+      </div>
+    );
+  };
 
-
- const  Summary = () =>{
-
-    return(
-        <div className='summary-main  common-wrapper '>
-            
-            <div className='left'> left</div>
-            <div className='right'>right </div>
-
-        </div>   
-    )
-}
-export default Summary
+  return (
+    <div className="summary-main  common-wrapper ">
+      <div className="left">
+        <div className="engagement-wrapper">
+          <div className="heading">
+            <div className="head-title">Engagement</div>
+            <div className="head-detail">
+              Lorem Ipsum eaecenas maximus urna congue urna congue.
+            </div>
+          </div>
+          <div className="main-detail">
+            {[0, 1, 2, 3].map((elevation) => (
+              <div className="">
+                <Item key={elevation} elevation={2}>
+                  {renderDataCard()}
+                </Item>
+              </div>
+            ))}
+            <div className="large">
+              <Item elevation={2}>
+                <div className="single-card">
+                  <div className="card-left">
+                    <div className="data-title">20%</div>
+                    <div className="data-detail">Lead conversion score</div>
+                  </div>
+                  <div className="card-right">
+                    <img src={CardImg} />
+                  </div>
+                </div>
+              </Item>
+            </div>
+            <div className="">
+              <Item elevation={2}>small</Item>
+            </div>
+            <div className="">
+              <Item elevation={2}>small</Item>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <div className="heading">
+          <div className="head-title">Lead opportunity</div>
+          <div className="head-detail">
+            Lorem Ipsum eaecenas maximus urna congue urna congue.
+          </div>
+        </div>
+        <div className="lead-detail">
+          <div className="predictor-wrapper">
+          <PredictorWidget />
+          </div>
+          <div className="large">
+              <Item elevation={2}>
+                <div className="single-card">
+                  <div className="card-left">
+                    <div className="data-title">~$80,000</div>
+                    <div className="data-detail">Potential revenue</div>
+                  </div>
+                  <div className="card-right">
+                    <img src={CardImg} />
+                  </div>
+                </div>
+              </Item>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Summary;
